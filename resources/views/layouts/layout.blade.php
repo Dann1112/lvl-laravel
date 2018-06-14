b21feb03fe31d4b3bc8e6f4cbdf7e23e7198f8a8
Daniel Manrique <juan_daniel1112@hotmail.com>
2018-06-13 22:25:58 -0500

    Correciones pagina de inicio

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,10 +4,37 @@
     @include('partials.header')
     <body>
         <!-- Div que permite desplazar el contenido debido al header flotante-->
-        <div class="container-fluid row d-none p-0 d-md-flex" style="height:70px; background-color: black">
+        <div class="container-fluid row" style="height:70px; background-color: black">
         </div>
-        <div class="container-fluid d-md-none p-0" style="height:250px; background-color:black">
-        </div>
+        <div class="col-12 d-md-none p-0">
+
+				<ul class="nav flex-column text-center" style="background-color: black">
+
+			<!-- Session Data (Login or User Info) -->
+			@if(Auth::check())
+				<li class="nav-item border-bottom" style="background-color: goldenrod; color:red">
+					<a class="nav-link" href="#"><img class="rounded-circle" src="/assets/img/generic.png" style="max-height: 90%">{{auth()->user()->username}}</a>
+				</li>
+			@endif
+
+					<li class="nav-item border-bottom" >
+						<a class="nav-link" href="{{route('home')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.home')</a>
+					</li>
+					<li class="nav-item border-bottom">
+						<a class="nav-link" href="{{route('standings')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.standings')</a>
+					</li>
+					<li class="nav-item border-bottom">
+						<a class="nav-link" href="{{route('players')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.players')</a>
+					</li>
+					<li class="nav-item border-bottom">
+						<a class="nav-link" href="#" style="color:white; text-decoration:none; font-weight: bold">@lang('header.ranking')</a>
+					</li>
+					<li class="nav-item border-bottom">
+						<a class="nav-link" href="#" style="color:white; text-decoration:none; font-weight: bold">@lang('header.contact')</a>
+					</li>
+				</ul>
+
+			</div>
         @yield('content')
     </body>
     @include('partials.footer')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -73,35 +73,7 @@
 					<img class="my-auto mx-auto" src="/assets/img/logos/logo2.png" style="max-width:70%; max-height:95%">
 			</div>
 
-			<div class="col-12 p-0">
-
-				<ul class="nav flex-column text-center" style="background-color: black">
-
-			<!-- Session Data (Login or User Info) -->
-			@if(Auth::check())
-				<li class="nav-item border-bottom" style="background-color: goldenrod; color:red">
-					<a class="nav-link" href="#"><img class="rounded-circle" src="/assets/img/generic.png" style="max-height: 90%">{{auth()->user()->username}}</a>
-				</li>
-			@endif
-
-					<li class="nav-item border-bottom" >
-						<a class="nav-link" href="{{route('home')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.home')</a>
-					</li>
-					<li class="nav-item border-bottom">
-						<a class="nav-link" href="{{route('standings')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.standings')</a>
-					</li>
-					<li class="nav-item border-bottom">
-						<a class="nav-link" href="{{route('players')}}" style="color:white; text-decoration:none; font-weight: bold">@lang('header.players')</a>
-					</li>
-					<li class="nav-item border-bottom">
-						<a class="nav-link" href="#" style="color:white; text-decoration:none; font-weight: bold">@lang('header.ranking')</a>
-					</li>
-					<li class="nav-item border-bottom">
-						<a class="nav-link" href="#" style="color:white; text-decoration:none; font-weight: bold">@lang('header.contact')</a>
-					</li>
-				</ul>
-
-			</div>
+			
 	
 			
 	
