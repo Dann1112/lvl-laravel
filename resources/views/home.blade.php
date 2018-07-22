6cf3bd306d0f762b3f7a7e1d49b1cd9bc5309965
Daniel Manrique <juan_daniel1112@hotmail.com>
2018-07-21 23:15:24 -0500

    Modificaciones Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container text-center my-3 py-3">
+<div class="container text-center my-3 py-3" style="background-color:black">
         <hr><hr>
-        <h1 style="word-break: break-all">LIGA VIRTUAL LATINOAMERICANA<h1>
-        <h2 class="my-2">La liga gamer. Tú liga.</h2>
+        <h1 style="word-break: break-all">.GMR ESPORTS<h1>
+        <h2 class="my-2">Tu momento es ahora</h2>
         <hr>
         <hr>
-        <h3 style="color:yellow">Ultima Actualizacion</h3>
+        <h3 style="color:yellow">ULTIMAS ACTUALIZACIONES</h3>
+        <h4>Julio 2018</h4>
+        <p style="color:white">
+          - La comunidad puede ver las estadísticas de sus equipos en "Clasificacion"<br>
+          - La comunidad puede realizar su registro en el portal, con el cual después podrá hacer uso de mayores funcionalidades
+        </p>
+      
         <h4>Junio 2018</h4>
         <p style="color:white">
           - La comunidad puede ver las estadísticas de sus equipos en "Clasificacion"<br>
