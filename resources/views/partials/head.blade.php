6d665fc3334cec146a7d30e8f372b67378077343
Daniel Manrique <juan_daniel1112@hotmail.com>
2018-07-24 01:07:59 -0500

    Google Analytics script

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -24,5 +24,16 @@
         @endpush
         @stack('scripts')
         @stack('stylesheets')
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122829501-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122829501-1');
+</script>
+
     
 </head>
