623623200eacc83b4b68a733c326fd4fec0c4c0e
Daniel Manrique <juan_daniel1112@hotmail.com>
2018-08-15 00:37:20 -0500

    Reintento equipos

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -102,8 +102,8 @@ class TeamController extends Controller
      */
     public function show(Team $team)
     {
-        $next_fixture = \App\Fixture::whereRaw('? home_team = ? or away_team = ? AND status = 0',$team->id)->oldest('date')->first();
-        $last_fixture = \App\Fixture::whereRaw('? home_team = ? or away_team = ? AND status = 0',$team->id)->latest('date')->first();
+        $next_fixture = \App\Fixture::whereRaw('home_team = ? or away_team = ? AND status = 0',$team->id,$team->id)->oldest('date')->first();
+        $last_fixture = \App\Fixture::whereRaw('home_team = ? or away_team = ? AND status = 0',$team->id,$team->id)->latest('date')->first();
         $next_home_team = null;
         $next_away_team = null;
         $last_home_team = null;
