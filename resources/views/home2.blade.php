bd7d090ec18fb733c12de4d27c3145fd2a502b04
Daniel Manrique <juan_daniel1112@hotmail.com>
2018-09-02 21:11:10 -0500

    Videos

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -118,16 +118,8 @@
 
   </div>
 
-
-
-
-
-
-
   <div class="col-12 order-1 col-lg-8  order-lg-2">
 
-      <div class="row">
-        <div class="col-12 col-md-6">
             <br>
             <hr>
             <h3>TheKillerXD19</h3><br><h5>GOLDEN LIONS</h5>
@@ -157,11 +149,5 @@
             </div>
 
           
-        </div>
-      </div>
-
-
-
-        
-      </div>
+    </div>
 @endsection
